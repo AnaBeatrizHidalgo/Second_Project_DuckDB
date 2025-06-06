0e2f8a3c4a49804beb3a9cd18ec13665dd359556
Asuka27 <hidalgo.anabeatriz@gmail.com>
2025-06-06 12:41:11 -0300

    Adicionando arquivos iniciais

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Second_Project_DuckDB
+
+Cenario selecionado é o A -> justificar pq eleindica que se deve usar o duckdb
---
+++ duckdb.py
@@ -0,0 +1,36 @@
+import duckdb
+
+# Creating the colunar table
+info_pais = duckdb.sql('''
+    CREATE TABLE info_pais 
+        (Country VARCHAR,
+         Year INTEGER,
+         Population INTEGER,
+         IDH NUMERICAL,
+         Eletricity NUMERICAL,
+         Sanitation NUMERICAL,
+         Life_expectancy  NUMERICAL,
+         GDP NUMERICAL,
+         Eletricity_Investimente NUMERICAL,
+         Health_Investimente NUMERICAL,
+         CO2_Emission NUMERICAL,
+         Power_Consumed NUMERICAL,
+         Power_Import NUMERICAL,
+         Renewble_Energy NUMERICAL,
+         Sector VARCHAR,
+         Sector_Descrition VARCHAR,
+         CO2_Emission_Sector NUMERICAL,
+         Power_Source VARCHAR,
+         Power_is_renewable  BOOLEAN,
+         Power_Generation NUMERICAL,
+         CO2_Emission_Power NUMERICAL,
+                       )''')
+
+# Importing a CSV file to in-memory database
+duckdb.sql("SELECT info_pais, COUNT(*) " \
+           "FROM read_csv('nosso.csv') " \
+           "WHERE info_pais IS NOT NULL " \
+           "GROUP BY info_pais " \
+           "ORDER BY info_pais").show()
+
+
